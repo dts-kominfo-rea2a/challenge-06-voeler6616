d04ef3b6942eb9dee9abe2d0b0eb73b1ac2ccb2b
voeler6616 <rvoeler6616@gmail.com>
2022-06-10 08:54:20 +0000

    feat: add logic for read file

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -20,52 +20,34 @@ let modifyFile3 = (val) => {
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
 const bacaData = (fnCallback) => {
-  // const wordResults = [];
-  // fs.readFile(file1, "utf-8", (err, data) => {
-  //   if (err) throw fnCallback(err);
-    
-  //   const word = JSON.parse(data).message.split(' ');
-    
-  //   if ( word.length > 2 ) {
-  //     return fnCallback('Tidak boleh lebih dari 1 kata', null);
-  //   } else {
-  //     wordResults.push(word[1]);
-  //   }
-  //   // console.log(wordResults);
-  //   fnCallback(null, wordResults);
-  // });
-
-  // Cara Lain
   const wordResults = [];
   fs.readFile(file1, "utf-8", (err, data1) => {
     if (err) throw fnCallback(err, null);
-    return data1;
-  });
 
-  fs.readFile(file2, "utf-8", (err, data2) => {
-    if (err) throw fnCallback(err, null);
-    return data2;
-  });
+    fs.readFile(file2, "utf-8", ( err, data2 ) => {
+      if ( err ) throw fnCallback( err, null );
 
-  fs.readFile(file3, "utf-8", (err, data3) => {
-    if (err) throw fnCallback(err, null);
-    return data3;
+      fs.read(file3, "utf-8", ( err, data3 ) => {
+        if ( err ) throw fnCallback( err, null );
+
+        const word1 = JSON.parse(data1).message.split(' ');
+        const word2 = JSON.parse(data2)[0].message.split(' ');
+        const word3 = JSON.parse(data3)[0].data.message.split(' ');
+
+        if (word1.length > 2 || word2.length > 2 || word3.length > 2) {
+          return fnCallback('Tidak boleh lebih dari 1 kata', null);
+        } else {
+          wordResults.push(word1[1]);
+          wordResults.push(word2[1]);
+          wordResults.push(word3[1]);
+        }
+
+        fnCallback(null, wordResults);
+      })
+    })
   });
-    
-    const word1 = JSON.parse(data1).message.split(' ');
-    const word2 = JSON.parse(data2)[0].message.split(' ');
-    const word3 = JSON.parse(data3)[0].data.message.split(' ');
-    
-    if ( word1.length > 2 || word2.length > 2 || word3.length > 2 ) {
-      return fnCallback('Tidak boleh lebih dari 1 kata', null);
-    } else {
-      wordResults.push(word1[1]);
-      wordResults.push(word2[1]);
-      wordResults.push(word3[1]);
-    }
-    // console.log(wordResults);
-    fnCallback(null, wordResults);
 };
+console.log(bacaData());
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
